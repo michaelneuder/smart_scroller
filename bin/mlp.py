95023affa1870f0e524784e06bd09d1b0d8f8296
michaelneuder <michael.neuder@gmail.com>
2017-09-07 19:04:01 -0600

    data

---
+++ bin/mlp.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+import tensorflow as tf
+import numpy as np
+np.set_printoptions(threshold=np.nan)
+import pandas as pd
+
+def main():
+    file_path = 
+
+
+
+
+
+if __name__ == '__main__':
+    main()
